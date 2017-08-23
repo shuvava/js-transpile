a37a892d29364a10794579d457ec85e30145b9a3
geek <keeper2002@mail.ru>
2017-08-23 06:01:47 -0700

    fixed gulp dependency issue

--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -1,24 +1,52 @@
 const JsBuild = require('js-transpile');
 const gulp = require('gulp');
 
-const jsbuild = new JsBuild({
-    lint: {
-        failAfterError: false,
-    },
-});
+const LIBS_PATH = './libs';
+const APPS_SRC_PATH = './src/**/*.js';
+const APPS_DEST_PATH = './dist';
+
+// set a variable telling us if we're building in release
+var isRelease = false;
+if (process.env.NODE_ENV && process.env.NODE_ENV === 'Release') {
+    isRelease = true;
+}
+
 const jsLibs = new JsBuild({
     path: {
-        dest: '/libs',
+        dest: LIBS_PATH,
+        src: '',
     },
-    useMinify: true,
+    useMinify: isRelease,
     useFlatten: true,
 });
 
-
-gulp.task('build', () => {
-    jsbuild.build();
+const jsbuild = new JsBuild({
+    path: {
+        src: APPS_SRC_PATH,
+        dest: APPS_DEST_PATH,
+    },
+    lint: {
+        failAfterError: false,
+    },
+    useMinify: isRelease,
 });
 
-gulp.task('copy:libraries', () => {
-    jsLibs.copylibrary();
+gulp.task('cleanup-apps', () => jsbuild.clean());
+gulp.task('cleanup-libs', () => jsLibs.clean());
+
+gulp.task('copy-libraries', ['cleanup-libs'], () => jsLibs.copylibrary());
+
+gulp.task('build', ['cleanup-apps'], () => jsbuild.build());
+
+gulp.task('transpile', ['build'], () => {
+    const jsTranspile = new JsBuild({
+        path: {
+            src: APPS_SRC_PATH,
+            dest: APPS_DEST_PATH,
+        },
+        lint: {
+            failAfterError: false,
+        },
+    });
+    return jsTranspile.watch();
 });
